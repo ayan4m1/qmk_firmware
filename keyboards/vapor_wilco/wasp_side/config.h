b19e34bf1d2024a52086a67a648f5b08e8d7b4ca
ayan4m1 <andrew@bulletlogic.com>
2022-01-14 23:21:31 -0500

    fix mistakes in keymap and add RGB lighting

--- keyboards/vapor_wilco/wasp_side/config.h
+++ keyboards/vapor_wilco/wasp_side/config.h
@@ -45,7 +45,10 @@ SOFTWARE.
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
+
+#define RGB_DI_PIN C7
+#define RGBLED_NUM 5
