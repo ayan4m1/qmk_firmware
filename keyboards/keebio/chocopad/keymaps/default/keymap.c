991cbf302d4dc920bc85ed864c3c1a90491c1bd7
ayan4m1 <andrew@bulletlogic.com>
2024-02-24 15:45:49 -0500

    start turning chocopad into a midi keyboard

--- keyboards/keebio/chocopad/keymaps/default/keymap.c
+++ keyboards/keebio/chocopad/keymaps/default/keymap.c
@@ -10,10 +10,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x4(
-    KC_PGUP,  KC_HOME,  KC_UP,    KC_END ,
-    KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-    MO(_FN2), KC_VOLU,  KC_MPLY,  KC_MPRV,
-    MO(_FN1), KC_VOLD,  KC_MUTE,  KC_MNXT
+    MI_E3,  MI_F3,  MI_Fs3,    MI_G3	 ,
+    MI_C3,  MI_Cs3,  MI_D3,  MI_Ds3,
+    MO(_FN2), KC_VOLU,  KC_MPLY,  MI_ON,
+    MO(_FN1), KC_VOLD,  KC_MUTE,  MI_OFF
   ),
   [_FN1] = LAYOUT_ortho_4x4(
     KC_ESC,   KC_P7,    KC_P8,    KC_P9,
